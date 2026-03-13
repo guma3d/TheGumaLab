f9e06dbd7238e77b02898f08da951d5382ed6127
Guma3D <guma3d@example.com>
2026-03-13 20:27:21 +0900

    (Guma3D) refactor: clean up test scripts in StockMaster_01

--- StockMaster_01/test_S0_verify.py
+++
@@ -1,5 +0,0 @@
-import yfinance as yf
-try:
-    print(yf.Ticker("446750.KS").history(period="1d"))
-except Exception as e:
-    print(e)
--- StockMaster_01/test_yf.py
+++
@@ -1,5 +0,0 @@
-import yfinance as yf
-try:
-    print(yf.Ticker("465540.KS").info.get("shortName"))
-except:
-    pass
